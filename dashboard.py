b7585ff9e258b1493e5b4cfce68a36fcf8b405d8
Cheick-o-diallo <cheickoumardiallo990@gmail.com>
2025-02-08 00:46:30 -1200

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,17 +9,10 @@ def show_dashboard(df):
 
     st.write("### Visualisation des données")
     # Charger le fichier CSV
-    #file_path = "chemin/vers/ton_fichier.csv"  # Remplace par ton chemin réel
-    #df = pd.read_csv(file_path)
-
-    # Convertir les prix en numérique (enlevant d'éventuels caractères non numériques)
-    #df['prix'] = pd.to_numeric(df['prix'], errors='coerce')
-
-    # Supprimer les valeurs NaN dans la colonne prix
-    #df = df.dropna(subset=['prix'])
-
-    # 1. Distribution des prix des chiens
-    if "prix" in df.columns :
+    file_path = "data/" 
+    selected_file = st.selectbox("Sélectionnez un dataset :", os.listdir(file_path))
+    if selected_file:
+        df = pd.read_csv(os.path.join(file_path, selected_file))
         plt.figure(figsize=(10, 5))
         sns.histplot(df['prix'], bins=30, kde=True, color='skyblue')
         plt.xlabel("Prix (CFA)")
@@ -27,23 +20,5 @@ def show_dashboard(df):
         plt.title("Distribution des prix des chiens")
         plt.grid(True)
         plt.show()
-
-    """# 2. Diagramme circulaire des annonces par localisation
-    location_counts = df['adresse'].value_counts().head(10)  # Top 10 localisations
-
-    plt.figure(figsize=(8, 8))
-    plt.pie(location_counts, labels=location_counts.index, autopct='%1.1f%%', colors=sns.color_palette("pastel"))
-    plt.title("Répartition des annonces par localisation (Top 10)")
-    plt.show()
-
-    # 3. Prix moyen des chiens par ville (Top 10 villes)
-    avg_price_per_city = df.groupby('adresse')['prix'].mean().sort_values(ascending=False).head(10)
-
-    plt.figure(figsize=(12, 6))
-    sns.barplot(x=avg_price_per_city.values, y=avg_price_per_city.index, palette="viridis")
-    plt.xlabel("Prix moyen (CFA)")
-    plt.ylabel("Localisation")
-    plt.title("Prix moyen des chiens par localisation (Top 10)")
-    plt.grid(axis="x")
     plt.show()"""
 
